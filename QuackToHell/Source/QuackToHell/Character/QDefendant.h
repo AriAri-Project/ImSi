18092c241287ef88c12e4401fbc85a1b3bc5c37a
Seohyun Ryu <atz999@ewhain.net>
2024-12-17 21:54:07 +0900

    feat: NPC 기본 구성

---
+++ QuackToHell/Source/QuackToHell/Character/QDefendant.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/QNonPlayer.h"
+#include "QDefendant.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUACKTOHELL_API AQDefendant : public AQNonPlayer
+{
+	GENERATED_BODY()
+	
+};
